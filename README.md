1b7686340ac111664e74c34d311c1d97c3c70464
MRyo-ie <e165738@ie.u-ryukyu.ac.jp>
2019-11-28 02:31:18 +0900

    【リファクタリング】

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # 図表抽出(Image extraction)
 
-
 このプログラムは以下のリポジトリ(MITライセンス)を改変して作成しています。
 
 [rishizek's repo](https://github.com/rishizek/tensorflow-deeplab-v3-plus).
@@ -8,34 +7,46 @@
 ## Setup
 TensorFlow (r1.6)以降と Python 3をお使いください。
 
-学習を試す場合は
-tensorflowの[slim](https://github.com/tensorflow/models/tree/master/research/slim)から[resnet_v2_50_2017_04_14.tar.gz](http://download.tensorflow.org/models/resnet_v2_50_2017_04_14.tar.gz)をダウンロードし、
-ini_checkpoints/resnet_v2_50
-に配置してください。
+## Predict
+1. model50ディレクトリを作る。
+2. [学習済重みファイル](http://lab.ndl.go.jp/dataset/trainedweights.zip)をダウンロード、解凍して、1. に配置する。
+3. 以下を実行。
+    ```bash
+    python3 picture_extraction.py --input_dir INPUT_DIR --output_dir OUTPUT_DIR
+    ```
 
-推論のみ試す場合は、model50ディレクトリ下に
-[学習済重みファイル](http://lab.ndl.go.jp/dataset/trainedweights.zip)を配置してください。
+## Train
+### データセット を準備
+1. pascal VOC形式の xml フォーマットで、
+    - 図表領域 ： "4_illustration"
+    - 資料全体 ： "1_overall"
+
+   のアノテーションを付与してください。
+
+2. 作成したxmlを`preprocess/annotxml`に、画像をpreprocess/imgに入れる。
+3. アノテーション結果画像を生成する。
+    ```
+    $ cd preprocess
+    $ python makeannotimage.py
+    ```
+    で、セグメンテーション画像ファイルが`preprocess/annotimg`に生成される。
 
 
-## Inference
-```bash
-python3 picture_extraction.py --input_dir INPUT_DIR --output_dir OUTPUT_DIR
-```
+## モデル（転移学習用） を準備
+1. `ini_checkpoints/resnet_v2_50` ディレクトリを作る。
+2. tensorflowの[slim](https://github.com/tensorflow/models/tree/master/research/slim)から[resnet_v2_50_2017_04_14.tar.gz](http://download.tensorflow.org/models/resnet_v2_50_2017_04_14.tar.gz) をダウンロードして、1. に配置する。
 
 
 ## Training
-pascal VOCのxmlのフォーマットで、図表部分の矩形領域に"4_illustration",資料全体の領域に"1_overall"のアノテーションを付与してください。
-作成したxmlをpreprocess/annotxmlに、画像をpreprocess/imgに入れ、
-preprocess/makeannotimage.py
-を実行すると、セグメンテーション画像ファイルがpreprocess/annotimgに生成されます。
-
-annotimg内にセグメンテーション画像が生成されたら、
-
 ```bash
-python3 create_pascal_tf_record.py
-python3 train_3class_101.py
+$ python3 create_pascal_tf_record.py
+$ python3 train_3class_50.py
 ```
 を実行すると学習が始まります。
+
+### 技術的な話
+- `create_pascal_tf_record.py` で TFRecode を作成している。
+- 
 
 
 
--- preprocess/makeannotimage.py
+++ preprocess/makeannotimage.py
@@ -1,61 +1,70 @@
+import cv2
+import glob
 import numpy as np
 import os
-import glob
 import pandas as pd
-import cv2
 import random
+from tqdm import tqdm
 from xml.etree import ElementTree as ET
 
+"""
+preprocess/ に cd してから実行しないと、エラーになる。
+"""
+
 random.seed(777)
+
 class XML_preprocessor(object):
 
-    def __init__(self, data_path):
-        self.path_prefix = data_path
+    def __init__(self):
+        self.xml_dir = 'annotxml'
         self.num_classes = 3
         self.data = dict()
         self._preprocess_XML()
 
     def _preprocess_XML(self):
-        filenames = glob.glob(self.path_prefix+"/*")
+        filenames = glob.glob(os.path.join(self.xml_dir, "*.xml"))
         random.shuffle(filenames)
         ft = open('train.txt', 'w')
         fv = open('val.txt', 'w')
-        for filename in filenames:
-            imgfilename=os.path.basename(filename[:-4])+".jpg"
+        for filename in tqdm(filenames):
+            # xml に対応する画像を探す。（画像の拡張子に対応するため。本当は png に統一したいけど...）
+            img_filepath = glob.glob(os.path.join('img', os.path.basename(filename[:-3]) + "*"))[0]
+            imgfilename = os.path.basename(img_filepath)
             tree = ET.parse(filename)
             root = tree.getroot()
             flag = False
             size_tree = root.find('size')
             width = int(size_tree.find('width').text)
             height = int(size_tree.find('height').text)
-            print(width,height)
-            annotimg=np.zeros((height, width, 1), np.uint8)
-            #o_xmin,o_xmax,o_ymin,o_ymax
+            annotimg = np.zeros((height, width, 1), np.uint8)
             for object_tree in root.findall('object'):
-                flag=True
+                flag = True
                 class_name = object_tree.find('name').text
                 xmin = int(object_tree.find("bndbox").find("xmin").text)
                 ymin = int(object_tree.find("bndbox").find("ymin").text)
                 xmax = int(object_tree.find("bndbox").find("xmax").text)
                 ymax = int(object_tree.find("bndbox").find("ymax").text)
-                if class_name=="4_illustration":
-                    for h in range(ymin, ymax):
-                        for w in range(xmin, xmax):
-                            annotimg[h, w] = max(1,annotimg[h, w])
-                elif class_name!="1_overall":
-                    for h in range(ymin, ymax):
-                        for w in range(xmin, xmax):
-                            annotimg[h, w] = 2
+                if class_name == "4_illustration":
+                    for y in range(ymin, ymax):
+                        for x in range(xmin, xmax):
+                            # 1_overall よりは下（隠れる）
+                            annotimg[y, x] = max(150, annotimg[y, x])
+                elif class_name != "1_overall":
+                    for y in range(ymin, ymax):
+                        for x in range(xmin, xmax):
+                            annotimg[y, x] = 200
                 else:
-                    o_xmin, o_xmax, o_ymin, o_ymax =xmin,xmax,ymin,ymax
-            if random.random()<0.1:
+                    o_xmin, o_xmax, o_ymin, o_ymax = xmin, xmax, ymin, ymax
+            if random.random() < 0.1:
                 fv.write(imgfilename + "\n")
             else:
                 ft.write(imgfilename + "\n")
-            cv2.imwrite(os.path.join("annotimg",imgfilename), annotimg)
+            cv2.imwrite(os.path.join("annotimg", imgfilename), annotimg)
         ft.close()
         fv.close()
 
-## example on how to use it
-import pickle
-XML_preprocessor('annotxml')
+
+if __name__ == "__main__":
+    # example on how to use it
+    import pickle
+    XML_preprocessor()
